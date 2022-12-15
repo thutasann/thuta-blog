8781b8c8b7b836baa3b681c413866c252f5a874a
thutasann <thutasann@gmail.com>
2022-12-15 13:12:33 +0630

    `nextjs sanity connect`

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,15 +3,16 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
+  basePath: "/studio",
   name: 'default',
   title: 'thuta-blog backend',
-
-  projectId: '66zlu2ry',
-  dataset: 'production',
-
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
