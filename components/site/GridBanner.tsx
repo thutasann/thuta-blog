e4da061002190ee25c7d044a1e7833f3738a78e4
thutasann <thutasann@gmail.com>
2022-12-22 14:27:12 +0630

    techImg style

--- components/site/GridBanner.tsx
+++ components/site/GridBanner.tsx
@@ -15,31 +15,31 @@ function GridBanner() {
                     alt="thuta sann blog reactjs"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>
             
             {/* NextJS */}
-            <div className='relative rounded-lg shadow-xl min-h-[250px] md:min-h-[400px] lg:min-h-[530px] col-span-3'>
+            <div className='relative rounded-lg shadow-xl min-h-[200px] md:min-h-[400px] lg:min-h-[530px] col-span-3'>
                 <Image
                     src="/banners/thutasann-nextjs.png"
                     fill
                     alt="thuta sann blog nextjs"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>
 
             {/* Siwft IOS */}
-            <div className='rounded-lg relative shadow-xl min-h-[192px] md:min-h-[380px] lg:min-h-[500px] row-span-2 col-span-2'>
+            <div className='rounded-lg relative shadow-xl min-h-[152px] md:min-h-[380px] lg:min-h-[500px] row-span-2 col-span-2'>
                 <Image
                     src="/banners/thutasann-swift.jpeg"
                     fill
                     alt="thuta sann blog Swift"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>
 
@@ -51,7 +51,7 @@ function GridBanner() {
                     alt="thuta sann blog Gatsby"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>
 
@@ -63,7 +63,7 @@ function GridBanner() {
                     alt="thuta sann blog Tailwind"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>
 
@@ -75,7 +75,7 @@ function GridBanner() {
                     alt="thuta sann blog Firebase"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>  
 
@@ -87,7 +87,7 @@ function GridBanner() {
                     alt="thuta sann blog Firebase"
                     priority
                     quality={100}
-                    className='rounded-lg'
+                    className='techImg'
                 />
             </div>
             
